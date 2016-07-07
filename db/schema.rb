64cf6d0d56dc3120beb496c06eefe1ad05f73aa4
haanh <ngohaanh764@gmail.com>
2016-07-07 16:28:11 +0700

    signUp done!

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160707032007) do
+ActiveRecord::Schema.define(version: 20160707080416) do
+
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string   "name",            limit: 255
+    t.string   "email",           limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "password_digest", limit: 255
   end
+
 end
